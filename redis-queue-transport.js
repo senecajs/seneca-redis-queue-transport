fffd5c8a42e5e215de034036b84350f0e75f8350
Josh Reeter <joshreeter@gmail.com>
2016-04-28 14:20:06 -0500

    @pelger fix introduced.

--- redis-queue-transport.js
+++ redis-queue-transport.js
@@ -1,3 +1,4 @@
+/* jshint asi: true */
 /* Copyright (c) 2015 Cristian Ianto, Richard Rodger MIT License */
 'use strict'
 
@@ -15,41 +16,43 @@ var internals = {
   }
 }
 
+
+
 module.exports = function (options) {
   var seneca = this
   var plugin = 'redis-queue-transport'
-
   var so = seneca.options()
-  internals.defaults.timeout = so.timeout ? (so.timeout - 555) : internals.defaults.timeout
+  var tu;
 
+  internals.defaults.timeout = so.timeout ? (so.timeout - 555) : internals.defaults.timeout
   options = seneca.util.deepextend(internals.defaults, so.transport, options)
 
-  var tu = seneca.export('transport/utils')
+  tu = seneca.export('transport/utils')
 
-  seneca.add({role: 'transport', hook: 'listen', type: 'redis-queue'}, hook_listen_redis)
-  seneca.add({role: 'transport', hook: 'client', type: 'redis-queue'}, hook_client_redis)
+  seneca.add({role: 'transport', hook: 'listen', type: 'redis-queue'}, hookListenRedis)
+  seneca.add({role: 'transport', hook: 'client', type: 'redis-queue'}, hookClientRedis)
 
-  function hook_listen_redis (args, done) {
+  function hookListenRedis (args, done) {
     var seneca = this
     var type = args.type
-    var listen_options = seneca.util.clean(_.extend({}, options[type], args))
+    var listenOptions = seneca.util.clean(_.extend({}, options[type], args))
+    var useTopic = args.topic || 'seneca_any';
+    var redisIn = Redis.createClient(listenOptions.port, listenOptions.host)
+    var redisOut = Redis.createClient(listenOptions.port, listenOptions.host)
 
-    var redis_in = Redis.createClient(listen_options.port, listen_options.host)
-    var redis_out = Redis.createClient(listen_options.port, listen_options.host)
+    handleEvents(redisIn)
+    handleEvents(redisOut)
 
-    handle_events(redis_in)
-    handle_events(redis_out)
-
-    function on_message (topic, msgstr) {
+    function onMessage (topic, msgstr) {
       var data = tu.parseJSON(seneca, 'listen-' + type, msgstr)
 
-      tu.handle_request(seneca, data, listen_options, function (out) {
+      tu.handle_request(seneca, data, listenOptions, function (out) {
         if (out === null) {
           return
         }
 
         var outstr = tu.stringifyJSON(seneca, 'listen-' + type, out)
-        redis_out.lpush(topic + '_res' + '/' + data.origin, outstr, function (err, reply) {
+        redisOut.lpush(topic + '_res' + '/' + data.origin, outstr, function (err, reply) {
           if (err) {
             seneca.log.error('transport', 'redis-queue', err)
           }
@@ -57,113 +60,97 @@ module.exports = function (options) {
       })
     }
 
-    tu.listen_topics(seneca, args, listen_options, function (topic) {
-      seneca.log.debug('listen', 'brpop', topic + '_act', listen_options, seneca)
-
-      var waiting = false
-      function next () {
-        waiting = true
-        redis_in.brpop(topic + '_act', 0, function (err, reply) {
-          if (err) {
-            seneca.log.error('transport', 'server-redis-brpop', err)
-            waiting = false
-            return
-          }
-          if (!reply.length) {
-            waiting = false
-            return
-          }
-          on_message(topic, reply[1])
-          setImmediate(next)
-        })
-      }
-      setImmediate(function () {
-        if (!waiting) {
-          next()
+    var blockingRead = function blockingRead(topic) {
+      redisIn.blpop(topic + '_act', 0, function (err, reply) {
+        if (err) { return seneca.log.error('transport', 'server-redis-brpop', err); }
+        if (reply && reply.length) {
+          onMessage(topic, reply[1])
         }
-      })
-    })
+        blockingRead(topic);
+      });
+    }
+    blockingRead(useTopic);
 
-    seneca.add('role:seneca,cmd:close', function (close_args, done) {
+    seneca.add('role:seneca,cmd:close', function (closeArgs, done) {
       var closer = this
-
-      redis_in.end(true)
-      redis_out.end(true)
-      closer.prior(close_args, done)
+      redisIn.end(true)
+      redisOut.end(true)
+      closer.prior(closeArgs, done)
     })
 
-    seneca.log.info('listen', 'open', listen_options, seneca)
+    seneca.log.info('listen', 'open', listenOptions, seneca)
 
     done()
   }
 
-  function hook_client_redis (args, clientdone) {
+
+
+  function hookClientRedis(args, clientdone) {
     var seneca = this
     var type = args.type
-    var client_options = seneca.util.clean(_.extend({}, options[type], args))
+    var clientOptions = seneca.util.clean(_.extend({}, options[type], args))
+    var useTopic = args.topic || 'seneca_any';
+    var redisIn;
+    var redisOut;
+    var subscriber;
 
-    tu.make_client(make_send, client_options, clientdone)
+    tu.make_client(makeSend, clientOptions, clientdone)
 
-    function make_send (spec, topic, send_done) {
-      var redis_in = Redis.createClient(client_options.port, client_options.host)
-      var redis_out = Redis.createClient(client_options.port, client_options.host)
+    redisIn = Redis.createClient(clientOptions.port, clientOptions.host)
+    redisOut = Redis.createClient(clientOptions.port, clientOptions.host)
+    subscriber = Redis.createClient(clientOptions.port, clientOptions.host)
 
-      handle_events(redis_in)
-      handle_events(redis_out)
+    handleEvents(redisIn)
+    handleEvents(redisOut)
 
-      function on_message (topic, msgstr) {
-        var input = tu.parseJSON(seneca, 'client-' + type, msgstr)
-        tu.handle_response(seneca, input, client_options)
-      }
 
-      seneca.log.debug('client', 'brpop', topic + '_res', client_options, seneca)
 
-      var waiting = false
-      function next () {
-        waiting = true
-        redis_in.brpop(topic + '_res' + '/' + seneca.id, 0, function (err, reply) {
-          if (err) {
-            seneca.log.error('transport', 'client-redis-brpop', err)
-            waiting = false
-            return
-          }
-          if (!reply.length) {
-            waiting = false
-            return
-          }
-          on_message(topic, reply[1])
-          setImmediate(next)
-        })
-      }
-      setImmediate(function () {
-        if (!waiting) {
-          next()
+    function onMessage (topic, msgstr) {
+      var input = tu.parseJSON(seneca, 'client-' + type, msgstr)
+      tu.handle_response(seneca, input, clientOptions)
+    }
+
+
+
+
+    var blockingRead = function blockingRead(topic) {
+      redisIn.brpop(topic + '_res' + '/' + seneca.id, 0, function (err, reply) {
+        if (err) { return seneca.log.error('transport', 'server-redis-brpop', err); }
+        if (reply && reply.length) {
+          onMessage(topic, reply[1])
         }
-      })
+        blockingRead(topic);
+      });
+    }
 
-      send_done(null, function (args, done) {
-        var outmsg = tu.prepare_request(this, args, done)
+    blockingRead(useTopic);
+
+
+
+    function makeSend(spec, topic, send_done) {
+      send_done(null, function (localArgs, done) {
+        var outmsg = tu.prepare_request(this, localArgs, done)
         var outstr = tu.stringifyJSON(seneca, 'client-' + type, outmsg)
 
-        redis_out.lpush(topic + '_act', outstr, function (err, reply) {
+        redisOut.rpush(useTopic + '_act', outstr, function (err, reply) {
           if (err) {
             seneca.log.error('transport', 'redis-queue', err)
           }
         })
       })
 
-      seneca.add('role:seneca,cmd:close', function (close_args, done) {
+      seneca.add('role:seneca, cmd:close', function (closeArgs, done) {
         var closer = this
-
-        redis_in.quit()
-        redis_out.quit()
-        closer.prior(close_args, done)
+        redisIn.quit()
+        redisOut.quit()
+        closer.prior(closeArgs, done)
       })
     }
   }
 
-  function handle_events (redisclient) {
-    // Die if you can't connect initially
+
+
+  function handleEvents(redisclient) {
     redisclient.once('ready', function () {
       redisclient.on('error', function (err) {
         seneca.log.error('transport', 'redis', err)
@@ -171,7 +158,10 @@ module.exports = function (options) {
     })
   }
 
+
+
   return {
     name: plugin
   }
 }
+
